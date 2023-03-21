50b010f6894aef3fada3156628d48bed796b1f9c
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2023-03-21 19:36:25 +0900

    bean scope prototype

--- src/main/java/com/example/spring5/Spring5Application.java
+++ src/main/java/com/example/spring5/Spring5Application.java
@@ -12,8 +12,12 @@ public class Spring5Application {
         ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
         MyXMLBean myXMLBean =  context.getBean("myXMLBean", MyXMLBean.class);
         System.out.println(myXMLBean);
+        MyXMLBean myXMLBeanq =  context.getBean("myXMLBean", MyXMLBean.class);
+        System.out.println(myXMLBean == myXMLBeanq);
 
         MyXMLBean myXMLBean1 =  context.getBean("myXMLBean1", MyXMLBean.class);
         System.out.println(myXMLBean1);
+        MyXMLBean myXMLBean2 =  context.getBean("myXMLBean1", MyXMLBean.class);
+        System.out.println(myXMLBean1 == myXMLBean2);
     }
 }
