a3ebde154fd0f0a7be21c85d8e89f1a8845a5ab8
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2023-03-28 01:25:35 +0900

    java based configuration

--- src/main/java/com/example/spring5/Spring5Application.java
+++ src/main/java/com/example/spring5/Spring5Application.java
@@ -1,7 +1,9 @@
 package com.example.spring5;
 
+import com.example.spring5.config.ApplicationConfiguration;
 import com.example.spring5.domain.repository.CompanyService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
@@ -9,10 +11,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Spring5Application {
 
     public static void main(String[] args) {
-
-        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
+        var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
             var companyService = context.getBean("companyService", CompanyService.class);
             System.out.println(companyService.findById(2));
         }
     }
-}
