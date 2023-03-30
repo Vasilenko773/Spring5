f766e7f6c862b6a329e0824d6a9c9d2c33c77d13
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2023-03-30 19:48:12 +0900

    @SpringBootApplication

--- src/main/java/com/example/spring5/core/config/ApplicationConfiguration.java
+++ src/main/java/com/example/spring5/core/config/ApplicationConfiguration.java
@@ -2,22 +2,11 @@ package com.example.spring5.core.config;
 
 import com.example.spring5.core.company.CompanyRepository;
 import com.example.spring5.core.company.ConnectionPool;
-import com.example.spring5.core.company.CrudRepository;
 import com.example.spring5.web.config.WebConfiguration;
 import org.springframework.context.annotation.*;
-import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.stereotype.Component;
 
 @Import(WebConfiguration.class)
 @Configuration()
-@PropertySource("classpath:application.properties")
-@ComponentScan(basePackages = "com.example.spring5",
-useDefaultFilters = false,
-includeFilters = {
-        @Filter(type = FilterType.ANNOTATION, value = Component.class),
-        @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
-        @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
-})
 public class ApplicationConfiguration {
 
 
