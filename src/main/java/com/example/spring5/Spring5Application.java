bd80b9cc85de6aecad342bfc15441c98abc30bd8
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2023-03-31 21:26:06 +0900

    Properties

--- src/main/java/com/example/spring5/Spring5Application.java
+++ src/main/java/com/example/spring5/Spring5Application.java
@@ -1,13 +1,17 @@
 package com.example.spring5;
 
+import com.example.spring5.core.company.ConnectionPool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Spring5Application {
 
     public static void main(String[] args) {
-      SpringApplication.run(Spring5Application.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(Spring5Application.class, args);
+        System.out.println(run.getBean("pool2", ConnectionPool.class));
+        System.out.println(run.getBean("connectionPool", ConnectionPool.class));
 
     }
 }
