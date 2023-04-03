ef6baba4cd9906868ea9a7ceba648f8c45691d51
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2023-04-04 01:15:53 +0900

    spring boot test

---
+++ src/test/java/com/example/spring5/core/company/CompanyServiceTest.java
@@ -0,0 +1,43 @@
+package com.example.spring5.core.company;
+
+import com.example.spring5.core.company.dto.CompanyReadDto;
+import com.example.spring5.core.lisner.entity.EntityEvent;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+
+@ExtendWith(MockitoExtension.class)
+class CompanyServiceTest {
+
+    private static final Integer COMPANY_ID = 1;
+
+    @Mock
+    private CrudRepository<Integer, Company> companyRepository;
+    @Mock
+    private ApplicationEventPublisher eventPublisher;
+    @InjectMocks
+    private CompanyService companyService;
+
+    @Test
+    void findById() {
+        Mockito.doReturn(Optional.of(new Company(COMPANY_ID)))
+                .when(companyRepository).findById(COMPANY_ID);
+
+        Optional<CompanyReadDto> byId = companyService.findById(COMPANY_ID);
+        var expected = Optional.of(new CompanyReadDto(COMPANY_ID));
+
+        assertTrue(byId.isPresent());
+        byId.ifPresent(actual -> assertEquals(expected, byId));
+        Mockito.verify(eventPublisher).publishEvent(any(EntityEvent.class));
+    }
+}
