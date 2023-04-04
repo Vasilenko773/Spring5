c0adb120a58e5a6c51be8090660b7b289a23f729
Vasilenko <vasilenko.lesnik2017@yandex.ru>
2023-04-05 01:13:55 +0900

    spring boot integration test custom annotation

--- src/test/java/com/example/spring5/integration/company/CompanyServiceIT.java
+++ src/test/java/com/example/spring5/integration/company/CompanyServiceIT.java
@@ -2,10 +2,9 @@ package com.example.spring5.integration.company;
 
 import com.example.spring5.core.company.CompanyService;
 import com.example.spring5.core.company.dto.CompanyReadDto;
+import com.example.spring5.integration.annotation.IT;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestExecutionListener;
 
 import java.util.Optional;
 
@@ -13,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-@SpringBootTest
+@IT
 public class CompanyServiceIT {
 
     private static final Integer COMPANY_ID = 1;
